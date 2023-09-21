7dfd3c8b7e5b59c23ecd5dc68542d8b8f02a36d7
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-09-21 12:34:34 +0200

    Hittade logiskt fel, börjat på meny för svårighetsgrader.

--- Program.cs
+++ Program.cs
@@ -5,25 +5,40 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            int index = random.Next(1, 21);
-            int guesses = 5;
+            Random randomEasy = new Random();
+            Random randomHard = new Random();
+            int winningNumEasy = random.Next(1, 11);
+            int winningNumMed = random.Next(1, 21);
+            int winningNumHard = random.Next(1, 31);
+            int guesses = 0;
+            Console.WriteLine("Hej! Välkommen till gissa numret, välj din svårighetsgrad:");
+            Console.WriteLine("[1]Enkel svårighetsgrad: Fem gissningar, nummer mellan 1-10.");
+            Console.WriteLine("[2]Medel svårighetsgrad: Tre gissningar, nummer mellan 1-20.");
+            Console.WriteLine("[3]Svår svårighetsgrad: En gissning, nummer mellan 1-30.");
             Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
-            
-            int nr = Convert.ToInt32(Console.ReadLine());
-            while (guesses < 5) 
-            {
-                Console.WriteLine("Skriv det nummer du vill gissa på:");
-                if (nr == index && guesses <= 5)
+
+                while (guesses != 5)
                 {
-                    Console.WriteLine("Grattis! Du gissade rätt! Rätt nummer var {0}!", index);
-                    break;
-                }
-                else if (nr != index && guesses < 5)
-                {
-                    Console.WriteLine("Nej, det var fel gissning, ");
+                    Console.WriteLine("Skriv det nummer du vill gissa på:");
+                    int nr = Convert.ToInt32(Console.ReadLine());
+                    if (nr == winningNumMed)
+                    {
+                        Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumMed);
+                        break;
+                    }
+                    else if (nr != winningNumMed && nr > winningNumMed)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för högt!");
+                    }
+                    else if (nr != winningNumMed && nr < winningNumMed)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    }
+
                 }
 
-            }
+            
+           
             Console.WriteLine("Tack för att du spelade!");
             Console.ReadKey();
 
