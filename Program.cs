fa3ef1f3a098438597679d7eac39020a47a4fcdd
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-10-06 11:22:58 +0200

    Tagit bort de överflödiga argumenten i metoderna för de olika svårighetsnivåerna samt satt dessa metoder till public static. Lagt till Guesses++ för att räkna antal gillsningar i easy och hard-metoderna för att räkna antal försök.

--- Program.cs
+++ Program.cs
@@ -49,17 +49,22 @@ namespace NumbersGame
             Console.WriteLine("[3] Svår svårighetsgrad: En gissning, nummer mellan 1-30.");
             Console.WriteLine("[4] Avsluta programmet.");
             int choice = Convert.ToInt32(Console.ReadLine());
-            if ((choice => 1 && choice < 5))
+            if ((choice >= 1 && choice < 5))
             {
                 switch (choice)
                 {
                     case 1:
+                        Program.Easy();
                         break;
                     case 2:
+                        Program.Medium();
                         break;
                     case 3:
+                        Program.Hard();
                         break;
                     case 4:
+                        Console.WriteLine("Tack för att du spelade!");
+                        Console.ReadKey();
                         break;
 
                 }
@@ -70,104 +75,90 @@ namespace NumbersGame
             }
 
 
-        } 
+        }
+        public static void Easy()
+        {
+            Random randomEasy = new Random();
+            int winningNumEasy = randomEasy.Next(1, 11);
+            int easyGuesses = 0;
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            while (easyGuesses != 5)
+            {
 
+                Console.WriteLine("Skriv det nummer du vill gissa på:");
+                int nr = Convert.ToInt32(Console.ReadLine());
+                if (nr == winningNumEasy)
+                {
+                    Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumEasy);
+                    break;
+                }
+                else if (nr != winningNumEasy && nr > winningNumEasy)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                    easyGuesses++;
+                }
+                else if (nr != winningNumEasy && nr < winningNumEasy)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    easyGuesses++;
+                }
+            }
+        }
+        public static void Medium()
+        {
+            Random randomMed = new Random();
+            int winningNumMed = randomMed.Next(1, 21);
+            int medGuesses = 0;
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            while (medGuesses != 5)
+            {
+                Console.WriteLine("Skriv det nummer du vill gissa på:");
+                int nr = Convert.ToInt32(Console.ReadLine());
+                if (nr == winningNumMed)
+                {
+                    Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumMed);
+                    break;
+                }
+                else if (nr != winningNumMed && nr > winningNumMed)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                }
+                else if (nr != winningNumMed && nr < winningNumMed)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                }
 
+            }
 
+        }
+        public static void Hard()
+        {
+            Random randomHard = new Random();
+            int winningNumHard = randomHard.Next(1, 11);
+            int hardGuesses = 0;
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            while (hardGuesses != 5)
+            {
+                Console.WriteLine("Skriv det nummer du vill gissa på:");
+                int nr = Convert.ToInt32(Console.ReadLine());
+                if (nr == winningNumHard)
+                {
+                    Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumHard);
+                    break;
+                }
+                else if (nr != winningNumHard && nr > winningNumHard)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                }
+                else if (nr != winningNumHard && nr < winningNumHard)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                }
 
+            }
 
-}
+        }
 
-
-
-
-        //    Console.WriteLine("Tack för att du spelade!");
-        //    Console.ReadKey();
-
-
-        //}
-        //void Easy(string[] args)
-        //{
-        //    Random randomEasy = new Random();
-        //    int winningNumEasy = randomEasy.Next(1, 11);
-        //    int easyGuesses = 0;
-        //    Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
-        //    while (easyGuesses != 5)
-        //    {
-        //        
-        //        Console.WriteLine("Skriv det nummer du vill gissa på:");
-        //        int nr = Convert.ToInt32(Console.ReadLine());
-        //        if (nr == winningNumEasy)
-        //        {
-        //            Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumEasy);
-        //            break;
-        //        }
-        //        else if (nr != winningNumEasy && nr > winningNumEasy)
-        //        {
-        //            Console.WriteLine("Tyvärr, du gissade för högt!");
-        //        }
-        //        else if (nr != winningNumEasy && nr < winningNumEasy)
-        //        {
-        //            Console.WriteLine("Tyvärr, du gissade för lågt!");
-        //        }
-        //    }
-        //}
-        //void Medium(string[] args)
-        //{
-        //    Random randomMed = new Random();
-        //    int winningNumMed = randomMed.Next(1, 11);
-        //    int medGuesses = 0;
-        //    Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
-        //    while (medGuesses != 5)
-        //    {
-        //        Console.WriteLine("Skriv det nummer du vill gissa på:");
-        //        int nr = Convert.ToInt32(Console.ReadLine());
-        //        if (nr == winningNumMed)
-        //        {
-        //            Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumMed);
-        //            break;
-        //        }
-        //        else if (nr != winningNumMed && nr > winningNumMed)
-        //        {
-        //            Console.WriteLine("Tyvärr, du gissade för högt!");
-        //        }
-        //        else if (nr != winningNumMed && nr < winningNumMed)
-        //        {
-        //            Console.WriteLine("Tyvärr, du gissade för lågt!");
-        //        }
-
-        //    }
-
-
-        //}
-        //void Hard(string[] args)
-        //{
-        //    Random randomHard = new Random();
-        //    int winningNumHard = randomHard.Next(1, 11);
-        //    int hardGuesses = 0;
-        //    Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
-        //    while (hardGuesses != 5)
-        //    {
-        //        Console.WriteLine("Skriv det nummer du vill gissa på:");
-        //        int nr = Convert.ToInt32(Console.ReadLine());
-        //        if (nr == winningNumHard)
-        //        {
-        //            Console.WriteLine("Woho, du gjorde det! Rätt nummer var {0}!", winningNumHard);
-        //            break;
-        //        }
-        //        else if (nr != winningNumHard && nr > winningNumHard)
-        //        {
-        //            Console.WriteLine("Tyvärr, du gissade för högt!");
-        //        }
-        //        else if (nr != winningNumHard && nr < winningNumHard)
-        //        {
-        //            Console.WriteLine("Tyvärr, du gissade för lågt!");
-        //        }
-
-        //    }
-
-
-        //}
 
     }
 }
